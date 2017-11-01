5cc779ff00ff21e7a901582f66ab4a1fb1ce319c
jzijin <jzijin@163.com>
2017-11-01 21:29:09 +0800

    修改了

--- test.cpp
+++ test.cpp
@@ -4,5 +4,6 @@ int main()
 	std::cout<<"hello Git to become a good code create"<<endl;
 	return 0;
 	std::cout<<"git branch"<<endl;
-	std::cout<<"git branch"<<endl;
 }
+	std::cout<<"git branch"<<endl;
+	std::cout<<"git branch"<<endl;
